908752d1e150f447d484bb5d4fbaefb35488562c
Det Sorthepharack <detvisiths@gmail.com>
2020-02-10 17:54:07 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -79,7 +79,6 @@ export default function() {
       <Auth>
         <Router>
           <div className="App">
-            {/* <Navbar /> */}
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
