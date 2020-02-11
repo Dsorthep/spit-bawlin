ebe9b9a193e51e8ee470453b80b8990a4f8fdbb3
dsorthep <detvisiths@yahoo.com>
2020-02-11 05:54:18 -0600

    deleted game files

--- routes/api/bulletin.js
+++ routes/api/bulletin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
@@ -13,3 +14,39 @@ router.route("/")
   .post(notesController.create)
   .get(resourcesController.findAll)
   .post(resourcesController.create);
+
+
+  // const express = require("express");
+  // const router = express.Router();
+  
+  // const Notes = require("../../models/Notes");
+  
+  
+  // router.get("/users", async (req, res) => {
+  //   try {
+  //     res.status(200).json({
+  //       data: Notes
+  //     });
+  //   } catch (err) {
+  //     res.status(404).json({
+  //       message: "Some error occured",
+  //       err
+  //     });
+  //   }
+  // });
+
+//   const Resources = require("../../models/Resources");
+
+// router.get("/users", async (req, res) => {
+//   try {
+//     res.status(200).json({
+//       data: Resources
+//     });
+//   } catch (err) {
+//     res.status(404).json({
+//       err
+//     });
+//   }
+// });
+  
+  // module.exports = router;
